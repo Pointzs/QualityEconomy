189380616a359d652eea90f81933ef813a09e34a
Pointzs <122045447+Pointzs@users.noreply.github.com>
2024-02-13 23:49:59 +0000

    Updated PlaceholderHook.java
    Made it so that the balance placeholder doesnt covert to 10e10 if you have 10,000,000,000 and instead shows 10b

--- src/main/java/com/imnotstable/qualityeconomy/hooks/PlaceholderHook.java
+++ src/main/java/com/imnotstable/qualityeconomy/hooks/PlaceholderHook.java
@@ -4,9 +4,7 @@ import com.imnotstable.qualityeconomy.QualityEconomy;
 import com.imnotstable.qualityeconomy.api.QualityEconomyAPI;
 import com.imnotstable.qualityeconomy.commands.BalanceTopCommand;
 import com.imnotstable.qualityeconomy.configuration.Configuration;
-import com.imnotstable.qualityeconomy.storage.StorageManager;
 import com.imnotstable.qualityeconomy.util.Debug;
-import com.imnotstable.qualityeconomy.util.Logger;
 import com.imnotstable.qualityeconomy.util.Misc;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import net.kyori.adventure.text.Component;
@@ -19,134 +17,144 @@ import java.util.List;
 import java.util.UUID;
 
 public class PlaceholderHook extends PlaceholderExpansion {
-  
-  public static boolean load() {
-    if (new PlaceholderHook().register()) {
-      Logger.log(Component.text("Successfully registered expansion with PlaceholderAPI", NamedTextColor.GREEN));
-      return true;
-    } else
-      Logger.log(Component.text("Failed to register expansion with PlaceholderAPI", NamedTextColor.RED));
-    return false;
-  }
-  
-  @Override
-  public boolean canRegister() {
-    return true;
-  }
-  
-  @Override
-  public @NotNull String getAuthor() {
-    return String.join(", ", QualityEconomy.getInstance().getDescription().getAuthors());
-  }
-  
-  @Override
-  public @NotNull String getIdentifier() {
-    return QualityEconomy.getInstance().getName();
-  }
-  
-  @Override
-  public @NotNull String getVersion() {
-    return QualityEconomy.getInstance().getDescription().getVersion();
-  }
-  
-  @Override
-  public boolean persist() {
-    return true;
-  }
-  
-  @Override
-  public @NotNull List<String> getPlaceholders() {
-    return List.of(
-      "balancetop_#<integer>",
-      "balance", "balance_<uuid>", "balance_<player>",
-      "cbalance_<currency>", "cbalance_<currency>_<uuid>", "cbalance_<currency>_<player>",
-      "isPayable", "isPayable_<uuid>", "isPayable_<player>",
-      "isRequestable", "isRequestable_<uuid>", "isRequestable_<player>"
-    );
-  }
-  
-  @Override
-  public String onPlaceholderRequest(Player player, @NotNull String input) {
-    String[] elements = input.split("_");
-    
-    switch (elements[0]) {
-      case "balancetop" -> {
-        if (elements.length == 2 && elements[1].startsWith("#")) {
-          try {
-            int place = Integer.parseInt(elements[1].substring(1)) - 1;
-            return BalanceTopCommand.orderedPlayerList.get(place).getUsername();
-          } catch (NumberFormatException exception) {
-            new Debug.QualityError("Invalid input for \"balancetop_#<integer>\": " + input, exception).log();
-          }
-        }
-      }
-      case "balance" -> {
-        UUID uuid = null;
-        if (elements.length == 2) {
-          if (Misc.isUUID(elements[1])) {
-            uuid = UUID.fromString(elements[1]);
-          } else {
-            uuid = Bukkit.getOfflinePlayer(elements[1]).getUniqueId();
-          }
-        } else if (elements.length == 1) {
-          uuid = player.getUniqueId();
-        }
-        if (uuid == null)
-          return null;
-        return String.valueOf(QualityEconomyAPI.getBalance(uuid));
-      }
-      case "cbalance" -> {
-        if (Configuration.areCustomCurrenciesEnabled())
-          return "Feature is disabled";
-        UUID uuid = null;
-        if (elements.length == 3) {
-          if (Misc.isUUID(elements[2])) {
-            uuid = UUID.fromString(elements[2]);
-          } else {
-            uuid = Bukkit.getOfflinePlayer(elements[2]).getUniqueId();
-          }
-        } else if (elements.length == 2) {
-          uuid = player.getUniqueId();
-        }
-        if (uuid == null)
-          return null;
-        if (!StorageManager.getActiveStorageType().getCurrencies().contains(elements[1]))
-          return null;
-        return String.valueOf(QualityEconomyAPI.getCustomBalance(uuid, elements[1]));
-      }
-      case "isPayable" -> {
-        UUID uuid = null;
-        if (elements.length == 2) {
-          if (Misc.isUUID(elements[1])) {
-            uuid = UUID.fromString(elements[1]);
-          } else {
-            uuid = Bukkit.getOfflinePlayer(elements[2]).getUniqueId();
-          }
-        } else if (elements.length == 1) {
-          uuid = player.getUniqueId();
-        }
-        if (uuid == null)
-          return null;
-        return String.valueOf(QualityEconomyAPI.isPayable(uuid));
-      }
-      case "isRequestable" -> {
-        UUID uuid = null;
-        if (elements.length == 2) {
-          if (Misc.isUUID(elements[1])) {
-            uuid = UUID.fromString(elements[1]);
-          } else {
-            uuid = Bukkit.getOfflinePlayer(elements[2]).getUniqueId();
-          }
-        } else if (elements.length == 1) {
-          uuid = player.getUniqueId();
-        }
-        if (uuid == null)
-          return null;
-        return String.valueOf(QualityEconomyAPI.isRequestable(uuid));
-      }
+
+    public static boolean load() {
+        if (new PlaceholderHook().register()) {
+            Logger.log(Component.text("Successfully registered expansion with PlaceholderAPI", NamedTextColor.GREEN));
+            return true;
+        } else
+            Logger.log(Component.text("Failed to register expansion with PlaceholderAPI", NamedTextColor.RED));
+        return false;
     }
-    
-    return null;
-  }
-  
+
+    @Override
+    public boolean canRegister() {
+        return true;
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return String.join(", ", QualityEconomy.getInstance().getDescription().getAuthors());
+    }
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return QualityEconomy.getInstance().getName();
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return QualityEconomy.getInstance().getDescription().getVersion();
+    }
+
+    @Override
+    public boolean persist() {
+        return true;
+    }
+
+    @Override
+    public @NotNull List<String> getPlaceholders() {
+        return List.of(
+                "balancetop_#<integer>",
+                "balance", "balance_<uuid>", "balance_<player>",
+                "cbalance_<currency>", "cbalance_<currency>_<uuid>", "cbalance_<currency>_<player>",
+                "isPayable", "isPayable_<uuid>", "isPayable_<player>",
+                "isRequestable", "isRequestable_<uuid>", "isRequestable_<player>"
+        );
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player, @NotNull String input) {
+        String[] elements = input.split("_");
+
+        switch (elements[0]) {
+            case "balancetop" -> {
+                if (elements.length == 2 && elements[1].startsWith("#")) {
+                    try {
+                        int place = Integer.parseInt(elements[1].substring(1)) - 1;
+                        return BalanceTopCommand.orderedPlayerList.get(place).getUsername();
+                    } catch (NumberFormatException exception) {
+                        new Debug.QualityError("Invalid input for \"balancetop_#<integer>\": " + input, exception).log();
+                    }
+                }
+            }
+            case "balance" -> {
+                UUID uuid = null;
+                if (elements.length == 2) {
+                    if (Misc.isUUID(elements[1])) {
+                        uuid = UUID.fromString(elements[1]);
+                    } else {
+                        uuid = Bukkit.getOfflinePlayer(elements[1]).getUniqueId();
+                    }
+                } else if (elements.length == 1) {
+                    uuid = player.getUniqueId();
+                }
+                if (uuid == null)
+                    return null;
+                double balance = QualityEconomyAPI.getBalance(uuid);
+                return formatBalance(balance);
+            }
+            case "cbalance" -> {
+                if (Configuration.areCustomCurrenciesEnabled())
+                    return "Feature is disabled";
+                UUID uuid = null;
+                if (elements.length == 3) {
+                    if (Misc.isUUID(elements[2])) {
+                        uuid = UUID.fromString(elements[2]);
+                    } else {
+                        uuid = Bukkit.getOfflinePlayer(elements[2]).getUniqueId();
+                    }
+                } else if (elements.length == 2) {
+                    uuid = player.getUniqueId();
+                }
+                if (uuid == null)
+                    return null;
+                if (!StorageManager.getActiveStorageType().getCurrencies().contains(elements[1]))
+                    return null;
+                return String.valueOf(QualityEconomyAPI.getCustomBalance(uuid, elements[1]));
+            }
+            case "isPayable" -> {
+                UUID uuid = null;
+                if (elements.length == 2) {
+                    if (Misc.isUUID(elements[1])) {
+                        uuid = UUID.fromString(elements[1]);
+                    } else {
+                        uuid = Bukkit.getOfflinePlayer(elements[2]).getUniqueId();
+                    }
+                } else if (elements.length == 1) {
+                    uuid = player.getUniqueId();
+                }
+                if (uuid == null)
+                    return null;
+                return String.valueOf(QualityEconomyAPI.isPayable(uuid));
+            }
+            case "isRequestable" -> {
+                UUID uuid = null;
+                if (elements.length == 2) {
+                    if (Misc.isUUID(elements[1])) {
+                        uuid = UUID.fromString(elements[1]);
+                    } else {
+                        uuid = Bukkit.getOfflinePlayer(elements[2]).getUniqueId();
+                    }
+                } else if (elements.length == 1) {
+                    uuid = player.getUniqueId();
+                }
+                if (uuid == null)
+                    return null;
+                return String.valueOf(QualityEconomyAPI.isRequestable(uuid));
+            }
+        }
+
+        return null;
+    }
+
+    private String formatBalance(double balance) {
+        String[] suffixes = {"", "k", "m", "b", "t"}; // Add more if needed
+        int suffixIndex = 0;
+        while (balance >= 1_000 && suffixIndex < suffixes.length - 1) {
+            balance /= 1_000;
+            suffixIndex++;
+        }
+        return String.format("%.2f%s", balance, suffixes[suffixIndex]);
+    }
 }
